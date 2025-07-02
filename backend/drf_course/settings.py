1ff3c71cf47873e6b896d345fc39b796333a1675
DHRUVGANGAL <dhruvgangal123@gmail.com>
2025-07-02 19:16:33 +0530

    fix

--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -11,12 +11,18 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
 
+if not DEBUG and not SECRET_KEY:
+    raise ValueError("SECRET_KEY is not set for production environment")
 
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 
